8db2e0b45303fe72d6ac2c9ab7fa85c9afbbad75
kralco <51504068+kralco@users.noreply.github.com>
2024-08-15 10:05:46 +0100

    Delete Dicee Challenge directory

--- Dicee Challenge/dicee.js
+++
@@ -1,8 +0,0 @@
-var img1Rand = Math.random();
-var img2Rand = Math.random();
-
-img1Rand = Math.floor(img1Rand * 6) + 1;
-img2Rand = Math.floor(img2Rand * 6) + 1;
-
-document.querySelector(".img1").setAttribute("src", "images/dice" + img1Rand + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + img2Rand + ".png");
